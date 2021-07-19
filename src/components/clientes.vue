d82551f99e7a87c5c4348d8c4b7ac43565ee8927
Tiago Martins Ferreira <tiagomf29@hotmail.com>
2021-07-18 23:00:00 -0300

    commit

--- src/components/clientes.vue
+++ src/components/clientes.vue
@@ -16,6 +16,7 @@
             <strong></strong>
             <table>         
             </table>
+        <button id="excluir">Excluir</button>    
         </div>             
     </div>
 </template>
@@ -28,6 +29,7 @@ export default {
     mounted() {
         this.acaoBotao();
         this.botaoPesquisa();
+        this.botaoExluir();
     },
     methods: {
         acaoBotao:function () {            
@@ -94,7 +96,7 @@ export default {
                       console.log(element);
                       linha.innerHTML = `
                            
-                        <td>${response[vlr].nome}</td>
+                        <td><input class="marca" type="checkbox" /></td><td>${response[vlr].id}</td><td>${response[vlr].nome}</td>
                            
                       `
                       document.querySelector("table").appendChild(linha);
@@ -174,6 +176,88 @@ export default {
                    this.listarPorNome();
                 }
           });           
+        },
+        exluirNomes:function(){
+
+                let linha = document.querySelectorAll("tr");
+                let contador = 0;
+                linha.forEach(e=>{
+                    console.log(e);
+                                                 
+                    if(linha[contador].children[0].children[0].checked == true){
+
+                        console.log("Excluindo Cliente de código"+linha[contador].children[1].innerHTML)
+
+                        $("table").empty();            
+                        let texto =  document.querySelector("strong");                
+                        texto.style.color= "black";                      
+                        let idCliente = +linha[contador].children[1].innerHTML;            
+                        $.ajax({
+                            headers: { 
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json' 
+                            },                
+                            url: "http://localhost:8080/api/excluir",
+                            type: "DELETE",
+                            data:JSON.stringify({
+                                "id": idCliente
+                            }),
+                            beforeSend : function(){
+                                document.querySelector("strong").innerHTML = "Realizando comunicação com o servidor de banco de dados AWS. Aguarde!" 
+                            },
+                            success:function(response){
+                                console.log(response);                    
+                               
+                                let edtNomeCliente = document.querySelector("input");
+                                edtNomeCliente.value = "";                                                                     
+                            }
+
+                        })
+                        .done(function(msg){
+                            console.log(msg)
+                            document.querySelector("strong").innerHTML ="";                                                          
+                        })
+                        .fail(function(jqXHR, textStatus, msg){
+                            console.log(msg);
+                            let texto =  document.querySelector("strong");
+                            texto.innerHTML = "Problema na comunicação com o banco de dados da AWS. Serviço pode estar parado!";
+                            texto.style.color= "red";                
+                        });
+
+
+                        }
+
+                    contador = contador+1;  
+
+                })              
+
+        },
+        botaoExluir:function(){
+
+            let excluir = document.getElementById("excluir");
+            excluir.addEventListener('click', e=>{
+                console.log(e);
+                swal.fire({
+                                title: 'Exclusão de registros!',
+                                text: "Tem certeza que deseja realizar a exclusão do(s) registro(s)?",
+                                icon: 'warning',
+                                showCancelButton: true,
+                                confirmButtonColor: '#3085d6',
+                                cancelButtonColor: '#d33',
+                                confirmButtonText: 'Sim'
+                                }).then((result) => {
+                                if (result.isConfirmed) {                                    
+                                    this.exluirNomes(); 
+                                    $("table").empty();                                   
+                                    swal.fire(
+                                    'Deletado!',
+                                    'Registro(s) Deletado(s) com sucesso!.',
+                                    'Sucesso'
+                                    )
+                                }
+                            })                               
+            });
+
         }
         
     },
@@ -192,10 +276,18 @@ export default {
       font-size: 15px;
   }
   td{
-      font-size: 20px;
+      font-size: 15px;
       color: blue;  
       border-bottom: 1px solid;    
   }
+
+tr{
+   font-size: 15px;
+   color: blue;  
+   border: 1px solid;    
+  
+}
+
   #principal{
       border: 3px solid;
       border-radius: 15px;
@@ -225,6 +317,7 @@ export default {
   #divTab{
     margin: auto auto auto auto; 
     width: 200px;
+    text-align: center;
   }
 
   hr{
@@ -308,5 +401,28 @@ h3{
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     font-size: 25px;
 }
+
+.marca{
+    width: 20px;
+}
+
+
+#excluir{
+  background-color: #f4511e;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  font-size: 16px;
+  margin: 4px 2px;
+  opacity: 0.6;
+  transition: 0.3s;
+  display: inline-block;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+#excluir:hover {opacity: 1}
 
 </style>
